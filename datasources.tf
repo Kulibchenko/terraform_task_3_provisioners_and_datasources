ca373eaffbf2e3d00fdb8f561c3e4f532e6c0c89
Kulib <kulibchenkovalera@gmail.com>
2025-02-14 17:55:42 +0200

    2nd commit

--- datasources.tf
+++ datasources.tf
@@ -1,35 +1,3 @@
-data "azurerm_resource_group" "resourcegroup" {
+data "azurerm_resource_group" "example" {
   name = "${var.prefix}-resources"
 }
-
-# resource "azurerm_public_ip" "example" {
-#   name                = "${var.prefix}-public-ip"
-#   resource_group_name = azurerm_resource_group.example.name
-#   location            = azurerm_resource_group.example.location
-#   allocation_method   = "Static"
-
-#   tags = {
-#     environment = "Staging"
-#   }
-# }
-
-# data "azurerm_virtual_network" "example" {
-#   name                = "production"
-#   resource_group_name = "networking"
-# }
-
-# data "azurerm_network_interface" "example" {
-#   name                = "acctest-nic"
-#   resource_group_name = "networking"
-# }
-
-# data "azurerm_subnet" "example" {
-#   name                 = "backend"
-#   virtual_network_name = "production"
-#   resource_group_name  = "networking"
-# }
-
-# data "azurerm_virtual_machine" "example" {
-#   name                = "production"
-#   resource_group_name = "networking"
-# }
